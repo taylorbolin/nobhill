25b99943228f11c919502267dd7f409302888af7
Taylor Bolin <t.n.bolin@gmail.com>
2016-04-17 11:29:28 -0700

    finishing touches on testimonials page

--- JS/script.js
+++ JS/script.js
@@ -142,192 +142,192 @@
   e.addEventListener ? e.addEventListener("resize", r, !1) : e.attachEvent(
     "onresize", r)
 }(window),
-function() {
-  var e, t;
-  e = function() {
-      function e(e, t) {
-        var i, o;
-        if (this.options = {
-          target: "instafeed",
-          get: "popular",
-          resolution: "thumbnail",
-          sortBy: "none",
-          links: !0,
-          mock: !1,
-          useHttp: !1
-        }, "object" == typeof e)
-          for (i in e) o = e[i], this.options[i] = o;
-        this.context = null != t ? t : this, this.unique = this._genKey()
-      }
-      return e.prototype.hasNext = function() {
-        return "string" == typeof this.context.nextUrl && this.context.nextUrl
-          .length > 0
-      }, e.prototype.next = function() {
-        return this.hasNext() ? this.run(this.context.nextUrl) : !1
-      }, e.prototype.run = function(t) {
-        var i, o, s;
-        if ("string" != typeof this.options.clientId && "string" != typeof this
-          .options.accessToken) throw new Error(
-          "Missing clientId or accessToken.");
-        if ("string" != typeof this.options.accessToken && "string" != typeof this
-          .options.clientId) throw new Error(
-          "Missing clientId or accessToken.");
-        return null != this.options.before && "function" == typeof this.options
-          .before && this.options.before.call(this), "undefined" != typeof document &&
-          null !== document && (s = document.createElement("script"), s.id =
-            "instafeed-fetcher", s.src = t || this._buildUrl(), i = document.getElementsByTagName(
-              "head"), i[0].appendChild(s), o = "instafeedCache" + this.unique,
-            window[o] = new e(this.options, this), window[o].unique = this.unique
-          ), !0
-      }, e.prototype.parse = function(e) {
-        var t, i, o, s, n, r, l, a, d, c, p, u, h, f, v, g, m, w, y, k, T, S;
-        if ("object" != typeof e) {
-          if (null != this.options.error && "function" == typeof this.options
-            .error) return this.options.error.call(this, "Invalid JSON data"), !
-            1;
-          throw new Error("Invalid JSON response")
-        }
-        if (200 !== e.meta.code) {
-          if (null != this.options.error && "function" == typeof this.options
-            .error) return this.options.error.call(this, e.meta.error_message), !
-            1;
-          throw new Error("Error from Instagram: " + e.meta.error_message)
-        }
-        if (0 === e.data.length) {
-          if (null != this.options.error && "function" == typeof this.options
-            .error) return this.options.error.call(this,
-            "No images were returned from Instagram"), !1;
-          throw new Error("No images were returned from Instagram")
-        }
-        if (null != this.options.success && "function" == typeof this.options
-          .success && this.options.success.call(this, e), this.context.nextUrl =
-          "", null != e.pagination && (this.context.nextUrl = e.pagination.next_url),
-          "none" !== this.options.sortBy) switch (f = "random" === this.options
-          .sortBy ? ["", "random"] : this.options.sortBy.split("-"), h =
-          "least" === f[0] ? !0 : !1, f[1]) {
-          case "random":
-            e.data.sort(function() {
-              return .5 - Math.random()
-            });
-            break;
-          case "recent":
-            e.data = this._sortBy(e.data, "created_time", h);
-            break;
-          case "liked":
-            e.data = this._sortBy(e.data, "likes.count", h);
-            break;
-          case "commented":
-            e.data = this._sortBy(e.data, "comments.count", h);
-            break;
-          default:
-            throw new Error("Invalid option for sortBy: '" + this.options.sortBy +
-              "'.")
-        }
-        if ("undefined" != typeof document && null !== document && this.options
-          .mock === !1) {
-          if (a = e.data, null != this.options.limit && a.length > this.options
-            .limit && (a = a.slice(0, this.options.limit + 1 || 9e9)), i =
-            document.createDocumentFragment(), null != this.options.filter &&
-            "function" == typeof this.options.filter && (a = this._filter(a,
-              this.options.filter)), null != this.options.template &&
-            "string" == typeof this.options.template) {
-            for (s = "", r = "", c = "", v = document.createElement("div"), g =
-              0, y = a.length; y > g; g++) n = a[g], l = n.images[this.options
-              .resolution].url, this.options.useHttp || (l = l.replace(
-              "http://", "//")), r = this._makeTemplate(this.options.template, {
-              model: n,
-              id: n.id,
-              link: n.link,
-              image: l,
-              caption: this._getObjectProperty(n, "caption.text"),
-              likes: n.likes.count,
-              comments: n.comments.count,
-              location: this._getObjectProperty(n, "location.name")
-            }), s += r;
-            for (v.innerHTML = s, S = [].slice.call(v.childNodes), m = 0, k =
-              S.length; k > m; m++) u = S[m], i.appendChild(u)
-          } else
-            for (w = 0, T = a.length; T > w; w++) n = a[w], d = document.createElement(
-                "img"), l = n.images[this.options.resolution].url, this.options
-              .useHttp || (l = l.replace("http://", "//")), d.src = l, this.options
-              .links === !0 ? (t = document.createElement("a"), t.href = n.link,
-                t.appendChild(d), i.appendChild(t)) : i.appendChild(d);
-          document.getElementById(this.options.target).appendChild(i), o =
-            document.getElementsByTagName("head")[0], o.removeChild(document.getElementById(
-              "instafeed-fetcher")), p = "instafeedCache" + this.unique,
-            window[p] = void 0;
-          try {
-            delete window[p]
-          } catch (b) {}
-        }
-        return null != this.options.after && "function" == typeof this.options
-          .after && this.options.after.call(this), !0
-      }, e.prototype._buildUrl = function() {
-        var e, t, i;
-        switch (e = "https://api.instagram.com/v1", this.options.get) {
-          case "popular":
-            t = "media/popular";
-            break;
-          case "tagged":
-            if ("string" != typeof this.options.tagName) throw new Error(
-              "No tag name specified. Use the 'tagName' option.");
-            t = "tags/" + this.options.tagName + "/media/recent";
-            break;
-          case "location":
-            if ("number" != typeof this.options.locationId) throw new Error(
-              "No location specified. Use the 'locationId' option.");
-            t = "locations/" + this.options.locationId + "/media/recent";
-            break;
-          case "user":
-            if ("number" != typeof this.options.userId) throw new Error(
-              "No user specified. Use the 'userId' option.");
-            if ("string" != typeof this.options.accessToken) throw new Error(
-              "No access token. Use the 'accessToken' option.");
-            t = "users/" + this.options.userId + "/media/recent";
-            break;
-          default:
-            throw new Error("Invalid option for get: '" + this.options.get +
-              "'.")
-        }
-        return i = "" + e + "/" + t, i += null != this.options.accessToken ?
-          "?access_token=" + this.options.accessToken : "?client_id=" + this.options
-          .clientId, null != this.options.limit && (i += "&count=" + this.options
-            .limit), i += "&callback=instafeedCache" + this.unique + ".parse"
-      }, e.prototype._genKey = function() {
-        var e;
-        return e = function() {
-          return (65536 * (1 + Math.random()) | 0).toString(16).substring(1)
-        }, "" + e() + e() + e() + e()
-      }, e.prototype._makeTemplate = function(e, t) {
-        var i, o, s, n, r;
-        for (o = /(?:\{{2})([\w\[\]\.]+)(?:\}{2})/, i = e; o.test(i);) s = i.match(
-            o)[1], n = null != (r = this._getObjectProperty(t, s)) ? r : "",
-          i = i.replace(o, "" + n);
-        return i
-      }, e.prototype._getObjectProperty = function(e, t) {
-        var i, o;
-        for (t = t.replace(/\[(\w+)\]/g, ".$1"), o = t.split("."); o.length;) {
-          if (i = o.shift(), !(null != e && i in e)) return null;
-          e = e[i]
-        }
-        return e
-      }, e.prototype._sortBy = function(e, t, i) {
-        var o;
-        return o = function(e, o) {
-          var s, n;
-          return s = this._getObjectProperty(e, t), n = this._getObjectProperty(
-            o, t), i ? s > n ? 1 : -1 : n > s ? 1 : -1
-        }, e.sort(o.bind(this)), e
-      }, e.prototype._filter = function(e, t) {
-        var i, o, s, n, r;
-        for (i = [], s = function(e) {
-          return t(e) ? i.push(e) : void 0
-        }, n = 0, r = e.length; r > n; n++) o = e[n], s(o);
-        return i
-      }, e
-    }(), t = "undefined" != typeof exports && null !== exports ? exports :
-    window, t.Instafeed = e
-}.call(this),
+// function() {
+//   var e, t;
+//   e = function() {
+//       function e(e, t) {
+//         var i, o;
+//         if (this.options = {
+//           target: "instafeed",
+//           get: "popular",
+//           resolution: "thumbnail",
+//           sortBy: "none",
+//           links: !0,
+//           mock: !1,
+//           useHttp: !1
+//         }, "object" == typeof e)
+//           for (i in e) o = e[i], this.options[i] = o;
+//         this.context = null != t ? t : this, this.unique = this._genKey()
+//       }
+//       return e.prototype.hasNext = function() {
+//         return "string" == typeof this.context.nextUrl && this.context.nextUrl
+//           .length > 0
+//       }, e.prototype.next = function() {
+//         return this.hasNext() ? this.run(this.context.nextUrl) : !1
+//       }, e.prototype.run = function(t) {
+//         var i, o, s;
+//         if ("string" != typeof this.options.clientId && "string" != typeof this
+//           .options.accessToken) throw new Error(
+//           "Missing clientId or accessToken.");
+//         if ("string" != typeof this.options.accessToken && "string" != typeof this
+//           .options.clientId) throw new Error(
+//           "Missing clientId or accessToken.");
+//         return null != this.options.before && "function" == typeof this.options
+//           .before && this.options.before.call(this), "undefined" != typeof document &&
+//           null !== document && (s = document.createElement("script"), s.id =
+//             "instafeed-fetcher", s.src = t || this._buildUrl(), i = document.getElementsByTagName(
+//               "head"), i[0].appendChild(s), o = "instafeedCache" + this.unique,
+//             window[o] = new e(this.options, this), window[o].unique = this.unique
+//           ), !0
+//       }, e.prototype.parse = function(e) {
+//         var t, i, o, s, n, r, l, a, d, c, p, u, h, f, v, g, m, w, y, k, T, S;
+//         if ("object" != typeof e) {
+//           if (null != this.options.error && "function" == typeof this.options
+//             .error) return this.options.error.call(this, "Invalid JSON data"), !
+//             1;
+//           throw new Error("Invalid JSON response")
+//         }
+//         if (200 !== e.meta.code) {
+//           if (null != this.options.error && "function" == typeof this.options
+//             .error) return this.options.error.call(this, e.meta.error_message), !
+//             1;
+//           throw new Error("Error from Instagram: " + e.meta.error_message)
+//         }
+//         if (0 === e.data.length) {
+//           if (null != this.options.error && "function" == typeof this.options
+//             .error) return this.options.error.call(this,
+//             "No images were returned from Instagram"), !1;
+//           throw new Error("No images were returned from Instagram")
+//         }
+//         if (null != this.options.success && "function" == typeof this.options
+//           .success && this.options.success.call(this, e), this.context.nextUrl =
+//           "", null != e.pagination && (this.context.nextUrl = e.pagination.next_url),
+//           "none" !== this.options.sortBy) switch (f = "random" === this.options
+//           .sortBy ? ["", "random"] : this.options.sortBy.split("-"), h =
+//           "least" === f[0] ? !0 : !1, f[1]) {
+//           case "random":
+//             e.data.sort(function() {
+//               return .5 - Math.random()
+//             });
+//             break;
+//           case "recent":
+//             e.data = this._sortBy(e.data, "created_time", h);
+//             break;
+//           case "liked":
+//             e.data = this._sortBy(e.data, "likes.count", h);
+//             break;
+//           case "commented":
+//             e.data = this._sortBy(e.data, "comments.count", h);
+//             break;
+//           default:
+//             throw new Error("Invalid option for sortBy: '" + this.options.sortBy +
+//               "'.")
+//         }
+//         if ("undefined" != typeof document && null !== document && this.options
+//           .mock === !1) {
+//           if (a = e.data, null != this.options.limit && a.length > this.options
+//             .limit && (a = a.slice(0, this.options.limit + 1 || 9e9)), i =
+//             document.createDocumentFragment(), null != this.options.filter &&
+//             "function" == typeof this.options.filter && (a = this._filter(a,
+//               this.options.filter)), null != this.options.template &&
+//             "string" == typeof this.options.template) {
+//             for (s = "", r = "", c = "", v = document.createElement("div"), g =
+//               0, y = a.length; y > g; g++) n = a[g], l = n.images[this.options
+//               .resolution].url, this.options.useHttp || (l = l.replace(
+//               "http://", "//")), r = this._makeTemplate(this.options.template, {
+//               model: n,
+//               id: n.id,
+//               link: n.link,
+//               image: l,
+//               caption: this._getObjectProperty(n, "caption.text"),
+//               likes: n.likes.count,
+//               comments: n.comments.count,
+//               location: this._getObjectProperty(n, "location.name")
+//             }), s += r;
+//             for (v.innerHTML = s, S = [].slice.call(v.childNodes), m = 0, k =
+//               S.length; k > m; m++) u = S[m], i.appendChild(u)
+//           } else
+//             for (w = 0, T = a.length; T > w; w++) n = a[w], d = document.createElement(
+//                 "img"), l = n.images[this.options.resolution].url, this.options
+//               .useHttp || (l = l.replace("http://", "//")), d.src = l, this.options
+//               .links === !0 ? (t = document.createElement("a"), t.href = n.link,
+//                 t.appendChild(d), i.appendChild(t)) : i.appendChild(d);
+//           document.getElementById(this.options.target).appendChild(i), o =
+//             document.getElementsByTagName("head")[0], o.removeChild(document.getElementById(
+//               "instafeed-fetcher")), p = "instafeedCache" + this.unique,
+//             window[p] = void 0;
+//           try {
+//             delete window[p]
+//           } catch (b) {}
+//         }
+//         return null != this.options.after && "function" == typeof this.options
+//           .after && this.options.after.call(this), !0
+//       }, e.prototype._buildUrl = function() {
+//         var e, t, i;
+//         switch (e = "https://api.instagram.com/v1", this.options.get) {
+//           case "popular":
+//             t = "media/popular";
+//             break;
+//           case "tagged":
+//             if ("string" != typeof this.options.tagName) throw new Error(
+//               "No tag name specified. Use the 'tagName' option.");
+//             t = "tags/" + this.options.tagName + "/media/recent";
+//             break;
+//           case "location":
+//             if ("number" != typeof this.options.locationId) throw new Error(
+//               "No location specified. Use the 'locationId' option.");
+//             t = "locations/" + this.options.locationId + "/media/recent";
+//             break;
+//           case "user":
+//             if ("number" != typeof this.options.userId) throw new Error(
+//               "No user specified. Use the 'userId' option.");
+//             if ("string" != typeof this.options.accessToken) throw new Error(
+//               "No access token. Use the 'accessToken' option.");
+//             t = "users/" + this.options.userId + "/media/recent";
+//             break;
+//           default:
+//             throw new Error("Invalid option for get: '" + this.options.get +
+//               "'.")
+//         }
+//         return i = "" + e + "/" + t, i += null != this.options.accessToken ?
+//           "?access_token=" + this.options.accessToken : "?client_id=" + this.options
+//           .clientId, null != this.options.limit && (i += "&count=" + this.options
+//             .limit), i += "&callback=instafeedCache" + this.unique + ".parse"
+//       }, e.prototype._genKey = function() {
+//         var e;
+//         return e = function() {
+//           return (65536 * (1 + Math.random()) | 0).toString(16).substring(1)
+//         }, "" + e() + e() + e() + e()
+//       }, e.prototype._makeTemplate = function(e, t) {
+//         var i, o, s, n, r;
+//         for (o = /(?:\{{2})([\w\[\]\.]+)(?:\}{2})/, i = e; o.test(i);) s = i.match(
+//             o)[1], n = null != (r = this._getObjectProperty(t, s)) ? r : "",
+//           i = i.replace(o, "" + n);
+//         return i
+//       }, e.prototype._getObjectProperty = function(e, t) {
+//         var i, o;
+//         for (t = t.replace(/\[(\w+)\]/g, ".$1"), o = t.split("."); o.length;) {
+//           if (i = o.shift(), !(null != e && i in e)) return null;
+//           e = e[i]
+//         }
+//         return e
+//       }, e.prototype._sortBy = function(e, t, i) {
+//         var o;
+//         return o = function(e, o) {
+//           var s, n;
+//           return s = this._getObjectProperty(e, t), n = this._getObjectProperty(
+//             o, t), i ? s > n ? 1 : -1 : n > s ? 1 : -1
+//         }, e.sort(o.bind(this)), e
+//       }, e.prototype._filter = function(e, t) {
+//         var i, o, s, n, r;
+//         for (i = [], s = function(e) {
+//           return t(e) ? i.push(e) : void 0
+//         }, n = 0, r = e.length; r > n; n++) o = e[n], s(o);
+//         return i
+//       }, e
+//     }(), t = "undefined" != typeof exports && null !== exports ? exports :
+//     window, t.Instafeed = e
+// }.call(this),
   function(e) {
     "use strict";
     "function" == typeof define && define.amd ? define(["jquery"], e) :
